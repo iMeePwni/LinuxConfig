cb736a28e1d4422dca39bd46ac034bdcbf0c76ba
feng.guo <feng.guo@bees360.com>
2021-11-19 17:55:14 +0800

    init base configi.

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+# Enable power manager.
+xfce4-power-manager &
+picom &
+slstatus &
+# Compat JAVA App.
+wmname LG3D &
+# Change Bg.
+$HOME/.script/auto-wall.sh &
+# Config ENV.
+export VISUAL=vim
+export EDITOR="$EDITOR"
